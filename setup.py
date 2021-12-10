71d98c91a7954b3aa3e2a7fe20a0f23c66e7bdb9
Harold Solbrig <solbrig@jhu.edu>
2021-12-10 11:08:18 -0600

    Update setup.py
    Test to see whether name makes github dependency graph work

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+# This needs to be here in order for the github dependency graph to work
+NAME = "hbreader"
+
 setup(
     setup_requires=['pbr'],
     pbr=True,
